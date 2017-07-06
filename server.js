b906d3a896e7d4b1c6659505a10fd4774d3cf36d
Amr Abdelraouf <ga47wuj@mytum.de>
2017-07-06 19:32:48 +0200

    create project skeleton

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/WDMDB');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
